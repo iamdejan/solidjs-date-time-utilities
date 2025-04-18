1b3b48f9b0e9e4db4ce9b21aae2a45203c0a4f59
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2025-04-18 22:08:32 +0700

    feat(gregorian-easter): add Copy Year button and search params

--- src/pages/GregorianEaster/GregorianEaster.tsx
+++ src/pages/GregorianEaster/GregorianEaster.tsx
@@ -1,11 +1,42 @@
-import { Typography } from "@suid/material";
-import { JSX, For } from "solid-js";
+import { Box, Button, Typography } from "@suid/material";
+import { JSX, For, createEffect } from "solid-js";
 import Tabs from "./Tabs";
 import getDisplayedYears from "./getDisplayedYears";
 import InformationTable from "./InformationTable";
+import { getRouteApi, useNavigate } from "@tanstack/solid-router";
+import useChosenYear from "./hooks/useChosenYear";
+import { useClipboard } from "solidjs-use";
 
 export default function GregorianEaster(): JSX.Element {
+  const routeSearch = getRouteApi("/gregorian-easter").useSearch();
+  const { copy } = useClipboard();
+
   const years = getDisplayedYears();
+  const chosenYear = useChosenYear((state) => state.chosenYear);
+  const setYear = useChosenYear((state) => state.setChosenYear);
+  const clearYear = useChosenYear((state) => state.clearYear);
+
+  const navigate = useNavigate();
+  createEffect(() => {
+    const foundYear = years.find((y) => y === routeSearch().year);
+    if (!foundYear) {
+      clearYear();
+      navigate({ to: "/gregorian-easter" });
+      return;
+    }
+
+    setYear(foundYear);
+  });
+
+  function copyYear(): void {
+    const link =
+      window.location.origin +
+      "/gregorian-easter?" +
+      new URLSearchParams({
+        year: chosenYear().toString(),
+      });
+    copy(link);
+  }
 
   return (
     <>
@@ -18,7 +49,22 @@ export default function GregorianEaster(): JSX.Element {
 
       <Tabs years={years} />
 
-      <For each={years}>{(year) => <InformationTable year={year} />}</For>
+      <Box
+        sx={{
+          padding: "1rem",
+        }}
+      >
+        <For each={years}>{(year) => <InformationTable year={year} />}</For>
+        <Button
+          type="button"
+          sx={{
+            marginTop: "2rem",
+          }}
+          onClick={copyYear}
+        >
+          Copy Year
+        </Button>
+      </Box>
     </>
   );
 }
